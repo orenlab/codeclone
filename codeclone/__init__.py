a4724c2e227a81e96e13bc933dc137f46cb81734
Den Rozhnovskiy <pytelemonbot@mail.ru>
2026-01-19 18:15:56 +0500

    chore: added package version stream

--- codeclone/__init__.py
+++ codeclone/__init__.py
@@ -5,3 +5,12 @@ focused on architectural duplication.
 Copyright (c) 2026 Den Rozhnovskiy
 Licensed under the MIT License.
 """
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("codeclone")
+except PackageNotFoundError:
+    __version__ = "dev"
+
+__all__ = ["__version__"]
